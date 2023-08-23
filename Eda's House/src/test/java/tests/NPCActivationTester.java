94dca70afd1b64fdf3d42b29a65c32f1c842bbb6
anfo-code <artem29040506000@gmail.com>
2023-08-23 19:10:30 +0200

    create tests for NPC Activation

--- Eda's House/src/test/java/tests/NPCActivationTester.java
+++ Eda's House/src/test/java/tests/NPCActivationTester.java
@@ -1,67 +1,62 @@
 package tests;
 
 import com.example.edashouse.controller.ActivationActions;
+import com.example.edashouse.controller.GameController;
 import com.example.edashouse.model.constants.Items;
 import com.example.edashouse.model.constants.characters.NonPlayableCharacters;
 import com.example.edashouse.model.constants.characters_data.ActionsConstants;
-import com.example.edashouse.model.utils.GameLogicHandler;
 import com.example.edashouse.view.Layout;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import utils_for_tests.TestUtils;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class NPCActivationTester {
     //TODO Check, if activated NPC gives appropriate items
-    /**
-     * The underlying logic is as follows: each NPC has its own "isToBeActivatedStatus"
-     * Therefore, I need to simulate, that called a NPC has it set to true
-     * <p>
-     * To achieve this goal, the test will be structured into the following steps:
-     * <p>
-     * - Simulate Layout
-     * <p>
-     * - Simulate character movement
-     * - Verify the outcomes of the simulated movement (coordinates and character direction)
-     * - Confirm the object designated for activation
-     * <p>
-     * - Simulate object activation
-     * - Validate the response from the activated object
-     */
+    private GameController gameController;
     @Mock
-    private Layout mockedLayout;
+    private Stage stage;
     @Mock
-    private ActivationActions mockedActivationActions;
+    private Scene scene;
     @Mock
-    private GameLogicHandler mockedGameLogicHandler;
+    private Layout layout;
 
-    private int outcome;
+    private ActivationActions activationActions;
+
+    @BeforeEach
+    private void init() {
+        stage = mock(Stage.class);
+        scene = mock(Scene.class);
+        gameController = new GameController();
+        gameController.startForTest(stage, scene);
+        layout = gameController.getLayout();
+        activationActions = gameController.getSceneListenersSetter().getActivationActions();
+    }
 
     @ParameterizedTest
     @CsvSource({"0, 0", "1, 0", "2, 0", "3, 0", "4, 0", "5, 1", "6, 0", "7, 0", "8, 2"})
     public void testRightNpcActivated(int npcToActivateCode, int expectedOutcome) {
         NonPlayableCharacters npc = TestUtils.getNPCFromNumber(npcToActivateCode);
-        assert npc != null;
+        Items expectedItem = getExpectedItemHeld(npc);
+        npc.setToBeActivated(true);
+        activationActions.receiveAction(ActionsConstants.F_KEY_PRESSED);
+
+        Assertions.assertEquals(expectedItem, layout.getWitch().getItemHeld());
+        layout.getWitch().setItemHeld(null);
+    }
+
+    private Items getExpectedItemHeld(NonPlayableCharacters npc) {
         Items expectedItem = npc.getItemDropped();
-
-        setMockedActivationActions();
-        setMockedGameLogicHandler(npc);
-
-        mockedActivationActions.receiveAction(ActionsConstants.F_KEY_PRESSED);
-
-    }
-
-    private void setMockedGameLogicHandler(NonPlayableCharacters npcToReturn) {
-        mockedGameLogicHandler = mock(GameLogicHandler.class);
-        when(mockedGameLogicHandler.getNPCToBeActivated()).thenReturn(npcToReturn);
-    }
-
-    //TODO finish this method by adding different conditions for
-    // methods called in ActivationActions.makeAnAction()
-    private void setMockedActivationActions() {
-        mockedActivationActions = mock(ActivationActions.class);
+        if (expectedItem != Items.EMPTY_ITEM) {
+            return expectedItem;
+        } else  {
+            return null;
+        }
     }
 }
