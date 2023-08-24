0f727532cd2469dfc8bca518445fbcc36095b52b
anfo-code <artem29040506000@gmail.com>
2023-08-24 18:18:17 +0200

    add snake's oil creation test

--- Eda's House/src/test/java/tests/SpellCreatingTester.java
+++ Eda's House/src/test/java/tests/SpellCreatingTester.java
@@ -37,7 +37,7 @@ public class SpellCreatingTester {
     }
 
     @ParameterizedTest
-    @CsvSource({"0, 4, 9, 2", "1, 0, 3, 7", "2, 0, 3, 1", "3, 1, 7, 3", "4, 4, 3, 2",})
+    @CsvSource({"0, 4, 9, 2", "1, 0, 3, 7", "2, 0, 3, 1", "3, 1, 7, 3", "4, 4, 3, 2"})
     public void testSpellCreated(int expectedPotionCode, int firstItemCode, int secondItemCode, int thirdItemCode) {
         Items[] itemsList = new Items[]{TestUtils.getItemFromNumber(firstItemCode),
                 TestUtils.getItemFromNumber(secondItemCode),TestUtils.getItemFromNumber(thirdItemCode)};
@@ -50,4 +50,20 @@ public class SpellCreatingTester {
         activationActions.interactWithPot();
         Assertions.assertEquals(TestUtils.getPotionFromNumber(expectedPotionCode), layout.getWitch().getPotionHeld());
     }
+
+    @ParameterizedTest
+    @CsvSource({"9, 6, 7, 0"})
+    public void testSnakesOilCreation(int expectedItemCode, int firstItemCode, int secondItemCode, int thirdItemCode) {
+        Items[] itemsList = new Items[]{TestUtils.getItemFromNumber(firstItemCode),
+                TestUtils.getItemFromNumber(secondItemCode),TestUtils.getItemFromNumber(thirdItemCode)};
+
+        for (Items item : itemsList){
+            layout.getWitch().setItemHeld(item);
+            activationActions.interactWithPot();
+        }
+
+        activationActions.interactWithPot();
+        Assertions.assertEquals(TestUtils.getItemFromNumber(expectedItemCode), layout.getWitch().getItemHeld());
+    }
+
 }
